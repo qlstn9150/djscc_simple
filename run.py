046317df77d6202d8e1cbce26e7808ef1840ab23
qlstn9150 <qlstn9150@gmail.com>
2022-01-07 09:02:33 +0900

    0107

--- run.py
+++ run.py
@@ -16,14 +16,14 @@ x_train, x_test = normalize_pixels(trainX, testX)
 
 
 #################### RUN ########################
-model = 'DJSCC-DN'
-model_f = DJSCC_DN
+model = 'DJSCC-Advanced'
+model_f = model8
 snr_train = [0, 10, 20]
 snr_test =  [2, 4, 7, 10, 13, 16, 18, 22, 25, 27] #[2, 10, 18, 26]
 compression_ratios = [0.06, 0.26, 0.49]
 
 ### TRAIN & EVALAUATION ###
-#train(model, model_f, snr_train, compression_ratios, x_train, x_test, batch_size=100, epochs=20)
+train(model, model_f, snr_train, compression_ratios, x_train, x_test, batch_size=100, epochs=20)
 #comp_eval(model, x_test, testX, compression_ratios, snr_train)
 #test_eval(model, x_test, testX, compression_ratios, snr_train, snr_test)
 
@@ -34,7 +34,7 @@ model_list = ['DJSCC',  'DJSCC-DN', 'DJSCC-Advanced']
 #mini_comp_plot('psnr', model_list, snr_train=[20], min=14, max=27, title='(SNR=20dB)')
 
 #comp_plot('psnr', model_list, snr_train)
-test_plot('psnr', model_list, snr_train, compression_ratios=[0.49])
+#test_plot('psnr', model_list, snr_train, compression_ratios=[0.49])
 #comp_plot('ssim', model_list, snr_train)
 #test_plot('ssim',model_list, snr_train, compression_ratios=[0.49])
 #all_img(model)
